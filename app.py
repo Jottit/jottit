ac32a2759cfd60751835f67b9ec1ecebef2b9b6f
Simon Carstensen <simonbc@gmail.com>
2026-03-07 21:25:22 +0100

    allow Cloudflare Analytics script and beacon in CSP headers

--- app.py
+++ app.py
@@ -40,7 +40,8 @@ def make_session_permanent():
 
 
 _CSP = (
-    "default-src 'self'; script-src 'self' https://cdn.jsdelivr.net; style-src 'self';"
+    "default-src 'self'; script-src 'self' https://cdn.jsdelivr.net https://static.cloudflareinsights.com;"
+    " connect-src 'self' https://cloudflareinsights.com; style-src 'self';"
     " img-src 'self' https://*.fly.storage.tigris.dev"
 )
 
