4b3acaf41d2f706de151a06d3e689540242d5b6e
Simon Carstensen <simonbc@gmail.com>
2026-03-08 11:36:08 +0100

    add migrations to drop sites table and add avatar/bio columns

---
+++ migrations/001_drop_sites.sql
@@ -0,0 +1,37 @@
+-- Migration: Drop sites table, move data to pages and users
+
+-- Add username and name columns to users
+ALTER TABLE users ADD COLUMN IF NOT EXISTS username TEXT UNIQUE;
+ALTER TABLE users ADD COLUMN IF NOT EXISTS name TEXT;
+
+-- Migrate site subdomain/title to users
+UPDATE users u
+SET username = s.subdomain, name = s.title
+FROM sites s
+WHERE s.user_id = u.id AND s.subdomain IS NOT NULL;
+
+-- Create new pages from sites (one page per site, using site slug)
+-- First, add user_id to pages
+ALTER TABLE pages ADD COLUMN IF NOT EXISTS user_id INTEGER REFERENCES users(id) ON DELETE SET NULL;
+
+-- For each site, set user_id on its pages and update slug to be the site slug
+-- Since the old model had site_id -> site.slug, we need to give each page a unique slug
+-- For index pages, use the site slug; for sub-pages, use siteslug-pageslug
+UPDATE pages p
+SET user_id = s.user_id
+FROM sites s
+WHERE p.site_id = s.id;
+
+-- Rename index pages to use the site slug
+UPDATE pages p
+SET slug = s.slug
+FROM sites s
+WHERE p.site_id = s.id AND p.slug = '-';
+
+-- For non-index pages, we can drop them (plan says no sub-pages)
+DELETE FROM pages WHERE slug = '-' OR slug LIKE '%';
+
+-- Drop old columns and tables
+ALTER TABLE pages DROP COLUMN IF EXISTS site_id;
+ALTER TABLE pages DROP COLUMN IF EXISTS nav_order;
+DROP TABLE IF EXISTS sites CASCADE;
