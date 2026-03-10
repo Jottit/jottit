8e57ec87470918ae1faa6ac6dcd0e7c5c9ad5553
Simon Carstensen <simonbc@gmail.com>
2026-03-10 15:10:50 +0100

    remove wikilinks from editor preview, remove page-actions top padding

--- static/editor.js
+++ static/editor.js
@@ -1,7 +1,6 @@
 var titleInput = document.querySelector('.editor-title');
 var contentInput = document.querySelector('.editor-content');
 var preview = document.getElementById('preview');
-var siteSlug = document.querySelector('.editor').dataset.slug;
 
 function smartypants(html) {
     var parts = html.split(/(<[^>]*>)/);
@@ -23,23 +22,12 @@ function smartypants(html) {
     return parts.join('');
 }
 
-function processWikilinks(text) {
-    return text.replace(/\[\[([^\[\]]+)\]\]/g, function(match, name) {
-        name = name.trim();
-        if (!name) return match;
-        var slug = name.toLowerCase().replace(/[^a-z0-9\s-]/g, '').replace(/\s+/g, '-').replace(/-+/g, '-').replace(/^-|-$/g, '');
-        if (!slug) return match;
-        var display = name.replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;');
-        return '<a href="/' + siteSlug + '/' + slug + '">' + display + '</a>';
-    });
-}
-
 function updatePreview() {
     var title = titleInput.value;
     var body = contentInput.value;
     var html = '';
     if (title) html += '<h1>' + title.replace(/&/g,'&amp;').replace(/</g,'&lt;') + '</h1>';
-    if (body) html += smartypants(marked.parse(processWikilinks(body)));
+    if (body) html += smartypants(marked.parse(body));
     preview.innerHTML = html;
 }
 
